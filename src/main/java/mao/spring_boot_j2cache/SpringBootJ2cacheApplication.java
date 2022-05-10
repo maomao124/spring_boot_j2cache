192c173344b59d255d86de11d00ca58bcc0d00bb
mao <1296193245@qq.com>
2022-05-10 21:23:48 +0800

    spring_boot_j2cache

---
+++ src/main/java/mao/spring_boot_j2cache/SpringBootJ2cacheApplication.java
@@ -0,0 +1,15 @@
+package mao.spring_boot_j2cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJ2cacheApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootJ2cacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/spring_boot_j2cache/SpringBootJ2cacheApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.spring_boot_j2cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJ2cacheApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
